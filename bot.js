9e1614c8bd176b9cefd7f59f760296c820a985f6
qwerty0s <qwerty0s.e.m@gmail.com>
2019-08-20 14:51:30 -0500

    Initial Commit at Version 2.0.0
    I should get better about using git for new projects.

---
+++ bot.js
@@ -0,0 +1,101 @@
+var Discord = require('discord.js');
+var auth = require('./auth.key');
+var fs = require('fs');
+var path = require('path');
+
+var bot = new Discord.Client();
+
+bot.once('ready', function(evt){
+
+	console.log("Connected as " + bot.user.username + " (" + bot.user.id + ")");
+
+	bot.user.setActivity("Uno™", {type: "PLAYING" });
+
+});
+
+bot.on('message', function(message){
+
+	if(message.author.bot == true){return;}
+
+	getLastAuthorID(message).then(function(id) {
+
+		var at = "<@" + id + ">";
+
+		var mess = message.content.toLowerCase().split(/\W+/gi);
+		var hasNoU = false;
+
+		for(var i = 0; i < mess.length; i++){
+
+			if(mess[i] == "u" || mess[i] == "you"){
+
+				if(i == 0){return;}
+
+				if(mess[i - 1] == "no"){
+
+					hasNoU = true;
+
+				}
+
+			}
+
+		}
+
+		if(hasNoU){
+
+			playReverse(message, at);
+
+		}
+
+	});
+
+});
+
+function playReverse(message, at, attachment){
+
+	var revFolder = path.join(__dirname, 'reverse');
+
+	fs.readdir(revFolder, function(err, files){
+
+		if(err){
+
+			console.log("error in func reverse: " + err);
+			return;
+
+		}
+
+		var card = "./reverse/" + files[Math.floor(Math.random() * files.length)];
+
+		message.channel.send(at, {
+
+			files:[{
+
+				attachment: card,
+				name: card.substring(10, card.length)
+
+			}]
+
+		});
+
+	});
+
+}
+
+function getLastAuthorID(message){
+
+	var id = message.channel.fetchMessages({limit: 50}).then(function(messages){
+
+		var Filtered = firstFiltered.filter(msg => !(msg.author.id == bot.user.id || msg.author.id == message.author.id || msg.createdTimestamp > message.createdTimestamp));
+
+		if(Filtered.first()){
+
+			return Filtered.first().author.id;
+
+		}
+
+	});
+
+	return id;
+
+}
+
+bot.login(auth.token);
